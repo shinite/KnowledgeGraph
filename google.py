f076659b788cec681f864299898a0a5f7719e973
shinite <ssshambhavi8@gmail.com>
2017-03-13 00:15:24 +0530

    Add files via upload

---
+++ google.py
@@ -0,0 +1,32 @@
+
+
+########### Python 2.7 #############
+import httplib, urllib, base64
+
+headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': '09448c2ffb4144eabfebbd6dfe0d0d35',
+}
+
+query =  raw_input()
+
+params = urllib.urlencode({
+    # Request parameters
+    'q': query,
+    'count': '1',
+    'offset': '0',
+    'mkt': 'en-us',
+    'safesearch': 'Moderate',
+})
+
+try:
+    conn = httplib.HTTPSConnection('api.cognitive.microsoft.com')
+    conn.request("GET", "/bing/v5.0/search?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
